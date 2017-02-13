278b0455880ecfba0eaa9e0039091a0058361134
taimoor <taimoorali@gmail.com>
2017-02-13 23:18:19 +0500

    Info Update

--- MSPL/Info/DepartmentInfo.vb
+++ MSPL/Info/DepartmentInfo.vb
@@ -2,4 +2,14 @@
     Public Property DEPT_ID As Integer
     Public Property Name As String
 
+    Sub New()
+
+    End Sub
+
+    Sub New(DepId As Integer, DepName As String)
+        DEPT_ID = DepId
+        Name = DepName
+    End Sub
+
+
 End Class
