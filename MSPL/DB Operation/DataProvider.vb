fc75cf868e3b8e9732dc308e278da172650a6338
taimoor <taimoorali@gmail.com>
2017-02-14 15:57:38 +0500

    new dboperation

---
+++ MSPL/DB Operation/DataProvider.vb
@@ -0,0 +1,29 @@
+﻿Imports Microsoft.Practices.EnterpriseLibrary.Data
+Imports Microsoft.Practices.EnterpriseLibrary.Common
+Public Class DataProvider
+    Sub New()
+    End Sub
+
+    Public Shared Function P_Department_GetAll() As DataTable
+        Dim objDatabase As Database
+        objDatabase = DatabaseFactory.CreateDatabase()
+        Return CType(objDatabase.ExecuteDataSet("P_Department_GetAll"), DataSet).Tables(0)
+    End Function
+    Public Shared Function P_Department_GetBy_Id(DEPT_ID As Integer) As DataRow
+        Dim objDatabase As Database
+        objDatabase = DatabaseFactory.CreateDatabase()
+        Return CType(objDatabase.ExecuteDataSet("P_Department_GetBy_Id", DEPT_ID), DataSet).Tables(0).Rows(0)
+    End Function
+    Public Shared Function P_Department_IU(Obj As DepartmentInfo) As Integer
+        Dim objDatabase As Database
+        objDatabase = DatabaseFactory.CreateDatabase()
+        Return CType(objDatabase.ExecuteDataSet("P_Department_IU", Obj.DEPT_ID, Obj.Name), DataSet).Tables(0).Rows(0).Item(0)
+    End Function
+
+    Public Shared Sub P_Department_Delete(DEPT_ID As Integer)
+        Dim objDatabase As Database
+        objDatabase = DatabaseFactory.CreateDatabase()
+        objDatabase.ExecuteNonQuery("P_Department_Delete", DEPT_ID)
+    End Sub
+
+End Class
