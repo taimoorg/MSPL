0951a5bf2ec464f112cb4e2a8ad4af9bffe23ff7
taimoor <taimoorali@gmail.com>
2017-02-15 23:50:09 +0500

    update

--- MSPL/Info/DepartmentInfo.vb
+++ MSPL/Info/DepartmentInfo.vb
@@ -1,14 +1,16 @@
 ﻿Public Class DepartmentInfo
     Public Property DEPT_ID As Integer
     Public Property Name As String
+    Public Property Last_Name As String
 
     Sub New()
 
     End Sub
 
-    Sub New(DepId As Integer, DepName As String)
+    Sub New(DepId As Integer, DepName As String, lastName As String)
         DEPT_ID = DepId
         Name = DepName
+        Last_Name = lastName
     End Sub
 
 
