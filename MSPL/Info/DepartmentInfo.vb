ce65a79736787967db5a2cccc173cce31a3a1d19
taimoor <taimoorali@gmail.com>
2017-02-20 22:30:01 +0500

    New Changes

--- MSPL/Info/DepartmentInfo.vb
+++ MSPL/Info/DepartmentInfo.vb
@@ -1,16 +1,16 @@
 ﻿Public Class DepartmentInfo
     Public Property DEPT_ID As Integer
-    Public Property Name As String
-    Public Property Last_Name As String
+    Public Property Dept_Name As String
+
 
     Sub New()
 
     End Sub
 
-    Sub New(DepId As Integer, DepName As String, lastName As String)
+    Sub New(DepId As Integer, DepName As String)
         DEPT_ID = DepId
-        Name = DepName
-        Last_Name = lastName
+        Dept_Name = DepName
+
     End Sub
 
 
