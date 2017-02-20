b728313663ac6b788039b2bfab83add1e616b0cf
taimoor <taimoorali@gmail.com>
2017-02-20 22:29:44 +0500

    New Changes

--- MSPL/DB Operation/DataProvider.vb
+++ MSPL/DB Operation/DataProvider.vb
@@ -18,7 +18,7 @@ Public Class DataProvider
     Public Shared Function P_Department_IU(obj As DepartmentInfo) As Integer
         Dim objDatabase As Database
         objDatabase = DatabaseFactory.CreateDatabase()
-        Return CType(objDatabase.ExecuteDataSet("P_Department_IU", obj.DEPT_ID, obj.Name, obj.Last_Name), DataSet).Tables(0).Rows(0).Item(0)
+        Return CType(objDatabase.ExecuteDataSet("P_Department_IU", obj.DEPT_ID, obj.Dept_Name), DataSet).Tables(0).Rows(0).Item(0)
     End Function
 
     Public Shared Sub P_Department_Delete(DEPT_ID As Integer)
@@ -27,18 +27,19 @@ Public Class DataProvider
         objDatabase.ExecuteNonQuery("P_Department_Delete", DEPT_ID)
     End Sub
 
-    Public Shared Function P_Department_GetBy_Name(Name As String, sOptions As Integer) As DataTable
+    Public Shared Function P_Department_GetBy_Name(Dept_Name As String, sOptions As Integer) As DataTable
         Dim objDatabase As Database
         objDatabase = DatabaseFactory.CreateDatabase()
         Select Case sOptions
             Case 1
-                Name = String.Format("{0}%", Name)
+                Dept_Name = String.Format("{0}%", Dept_Name)
             Case 2
-                Name = String.Format("%{0}%", Name)
+                Dept_Name = String.Format("%{0}%", Dept_Name)
             Case 3
-                Name = String.Format("%{0}", Name)
+                Dept_Name = String.Format("%{0}", Dept_Name)
 
         End Select
-        Return CType(objDatabase.ExecuteDataSet("P_Department_GetBy_Name", Name), DataSet).Tables(0)
+
+        Return CType(objDatabase.ExecuteDataSet("P_Department_GetBy_Name", Dept_Name), DataSet).Tables(0)
     End Function
 End Class
