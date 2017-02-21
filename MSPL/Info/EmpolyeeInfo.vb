932ac076a60f89bd057421f902099d6b37886ff3
taimoor <taimoorali@gmail.com>
2017-02-21 23:01:20 +0500

    Info Update

---
+++ MSPL/Info/EmpolyeeInfo.vb
@@ -0,0 +1,15 @@
+﻿
+Public Class EmpolyeeInfo
+    Public Property Emp_ID As Integer
+    Public Property Emp_Name As String
+    Public Property Emp_Address As String
+
+    Sub New(EmpId As Integer, EmpName As String, EmpAddress As String)
+        Emp_ID = EmpId
+        Emp_Name = EmpName
+        Emp_Address = EmpAddress
+
+
+    End Sub
+
+End Class
