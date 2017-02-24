e22c2cb2f0001648f8a58a9102eca80f622e5fc0
taimoor <taimoorali@gmail.com>
2017-02-24 23:08:58 +0500

    Info Update

--- MSPL/Info/EmpolyeeInfo.vb
+++ MSPL/Info/EmpolyeeInfo.vb
@@ -3,12 +3,17 @@ Public Class EmpolyeeInfo
     Public Property Emp_ID As Integer
     Public Property Emp_Name As String
     Public Property Emp_Address As String
+    Public Property DEPT_ID As Integer
 
-    Sub New(EmpId As Integer, EmpName As String, EmpAddress As String)
+    Sub New()
+
+    End Sub
+
+    Sub New(EmpId As Integer, EmpName As String, EmpAddress As String, DeptId As Integer)
         Emp_ID = EmpId
         Emp_Name = EmpName
         Emp_Address = EmpAddress
-
+        DEPT_ID = DeptId
 
     End Sub
 
