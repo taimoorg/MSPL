2ec691d28485415ab69d9b97f16f5861e7696dfe
taimoor <taimoorali504@gmail.com>
2017-04-10 18:58:25 +0500

    NEW UPDATES

--- MSPL/Info/EmpolyeeInfo.vb
+++ MSPL/Info/EmpolyeeInfo.vb
@@ -4,21 +4,23 @@ Public Class EmpolyeeInfo
     Public Property Emp_Name As String
     Public Property Emp_Address As String
     Public Property DEPT_ID As Integer
-    Public Property Hire_Date As Date
+    Public Property Hire_Date As String
     Public Property Emp_Shift As String
+    Public Property Salary As String
 
     Sub New()
 
     End Sub
 
-    Sub New(EmpId As Integer, EmpName As String, EmpAddress As String, DeptId As Integer, Hiredate As Date, shift As String)
+    Sub New(EmpId As Integer, EmpName As String, EmpAddress As String, DeptId As Integer, Hiredate As String, shift As String, EmpSalary As String)
+
         Emp_ID = EmpId
         Emp_Name = EmpName
         Emp_Address = EmpAddress
         DEPT_ID = DeptId
         Hire_Date = Hiredate
         Emp_Shift = shift
-
+        Salary = EmpSalary
     End Sub
 
 End Class
