6c8916515d4f8cb2851e522ce7ddc4741f04f41c
taimoor <taimoorali504@gmail.com>
2017-10-17 18:10:12 +0500

    sku DB

---
+++ MSPL/DB Operation/SkuDataProvider.vb
@@ -0,0 +1,9 @@
+﻿Imports Microsoft.Practices.EnterpriseLibrary.Data
+Imports Microsoft.Practices.EnterpriseLibrary.Common
+Public Class SkuDataProvider
+
+    Public Shared Sub P_Sku_IU(Sku_ID As Integer, Sku As String, Description As String)
+        Dim database As Database = DatabaseFactory.CreateDatabase()
+        database.ExecuteNonQuery("P_Sku_IU", Sku_ID, Sku, Description)
+    End Sub
+End Class
