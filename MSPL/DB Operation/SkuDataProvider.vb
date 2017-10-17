989cf10316842de69d74b75a85052dd017e33758
taimoor <taimoorali504@gmail.com>
2017-10-17 23:24:39 +0500

    db

--- MSPL/DB Operation/SkuDataProvider.vb
+++ MSPL/DB Operation/SkuDataProvider.vb
@@ -1,9 +1,20 @@
 ﻿Imports Microsoft.Practices.EnterpriseLibrary.Data
 Imports Microsoft.Practices.EnterpriseLibrary.Common
 Public Class SkuDataProvider
+    Sub New()
 
-    Public Shared Sub P_Sku_IU(Sku_ID As Integer, Sku As String, Description As String)
-        Dim database As Database = DatabaseFactory.CreateDatabase()
-        database.ExecuteNonQuery("P_Sku_IU", Sku_ID, Sku, Description)
     End Sub
+
+    Public Shared Sub P_Sku_IU(Sku_ID As Integer, SKU As String, Description As String, SumUPWeight As Integer, SumUPHeight As Integer, SumUPWidth As Integer, SumUPDepth As Integer)
+        Dim objdatabase As Database
+        objdatabase = DatabaseFactory.CreateDatabase()
+        objdatabase.ExecuteNonQuery("P_Sku_IU", Sku_ID, SKU, Description, SumUPWeight, SumUPHeight, SumUPWidth, SumUPDepth)
+    End Sub
+
+    Public Shared Function P_Sku_GetAll() As DataTable
+        Dim objDatabase As Database
+        objDatabase = DatabaseFactory.CreateDatabase()
+        Return CType(objDatabase.ExecuteDataSet("P_Sku_GetAll"), DataSet).Tables(0)
+    End Function
+
 End Class
