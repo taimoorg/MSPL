069b93c1961d49c908b8933ca132e9b8dac7a929
taimoor <taimoorali504@gmail.com>
2017-11-01 18:52:22 +0500

    Sku Data provider

--- MSPL/DB Operation/SkuDataProvider.vb
+++ MSPL/DB Operation/SkuDataProvider.vb
@@ -3,10 +3,10 @@ Imports Microsoft.Practices.EnterpriseLibrary.Common
 Public Class SkuDataProvider
     Sub New()
     End Sub
-    Public Shared Sub P_Sku_IU(Sku_ID As Integer, SKU As String, Description As String, SumUPWeight As Integer, SumUPHeight As Integer, SumUPWidth As Integer, SumUPDepth As Integer)
+    Public Shared Sub P_Sku_IU(Sku_ID As Integer, Client_ID As Integer, SKU As String, Description As String, SumUPWeight As Integer, SumUPHeight As Integer, SumUPWidth As Integer, SumUPDepth As Integer)
         Dim objdatabase As Database
         objdatabase = DatabaseFactory.CreateDatabase()
-        objdatabase.ExecuteNonQuery("P_Sku_IU", Sku_ID, SKU, Description, SumUPWeight, SumUPHeight, SumUPWidth, SumUPDepth)
+        objdatabase.ExecuteNonQuery("P_Sku_IU", Sku_ID, Client_ID, SKU, Description, SumUPWeight, SumUPHeight, SumUPWidth, SumUPDepth)
     End Sub
     Public Shared Function P_Sku_GetAll(ID As Integer) As DataTable
         Dim objDatabase As Database
@@ -18,11 +18,22 @@ Public Class SkuDataProvider
         database = DatabaseFactory.CreateDatabase()
         database.ExecuteNonQuery("P_Sku_Delete", Sku_ID)
     End Sub
+    Public Shared Function P_Sku_DuplicationCheck(Client_id As Integer, sku_id As Integer, sku As String) As Boolean
+        Dim database As Database
+        database = DatabaseFactory.CreateDatabase()
+        Return CType(database.ExecuteDataSet("P_Sku_DuplicationCheck", Client_id, sku_id, sku), DataSet).Tables(0).Rows(0).Item(0)
+    End Function
+    Public Shared Function P_Sku_DuplicationEditCheck(Sku_ID As Integer, Sku As String) As Boolean
+        Dim database As Database
+        database = DatabaseFactory.CreateDatabase()
+        Return CType(database.ExecuteDataSet("P_Sku_DuplicationEditCheck", Sku_ID, Sku), DataSet).Tables(0).Rows(0).Item(0)
+    End Function
+
     Public Shared Function ExecuteSelectSingleRow(ByVal Query As String) As System.Data.DataRow
-        Dim objdatabase As Database
+        Dim objDatabase As Database
         Dim DT As DataTable
-        objdatabase = DatabaseFactory.CreateDatabase()
-        DT = CType(objdatabase.ExecuteDataSet(System.Data.CommandType.Text, Query), System.Data.DataSet).Tables(0)
+        objDatabase = DatabaseFactory.CreateDatabase()
+        DT = CType(objDatabase.ExecuteDataSet(System.Data.CommandType.Text, Query), System.Data.DataSet).Tables(0)
         If DT.Rows.Count = 0 Then
             Return Nothing
         Else
