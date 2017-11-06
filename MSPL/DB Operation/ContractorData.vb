033a3e9221c754dc86c4a8ddfd99eeee0c085ea5
taimoor <taimoorali504@gmail.com>
2017-11-06 17:49:52 +0500

    da

---
+++ MSPL/DB Operation/ContractorData.vb
@@ -0,0 +1,10 @@
+﻿Imports Microsoft.Practices.EnterpriseLibrary.Data
+Imports Microsoft.Practices.EnterpriseLibrary.Common
+Public Class ContractorData
+
+    Public Shared Function P_Driver_GetAll() As DataTable
+        Dim database As Database
+        database = DatabaseFactory.CreateDatabase()
+        Return CType(database.ExecuteDataSet("P_Driver_GetAll"), DataSet).Tables(0)
+    End Function
+End Class
